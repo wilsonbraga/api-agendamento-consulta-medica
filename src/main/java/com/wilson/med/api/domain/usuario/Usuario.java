f18d6b8dcbc3fc1a07b4c8eb3c3c4cec1bda43d5
wilsonbraga <wilsonrodriguesbraga@gmail.com>
2023-07-18 22:14:22 -0300

    Classe Usuario e migration create-table-usuario sql

---
+++ src/main/java/com/wilson/med/api/domain/usuario/Usuario.java
@@ -0,0 +1,29 @@
+package com.wilson.med.api.domain.usuario;
+
+import com.wilson.med.api.domain.endereco.Endereco;
+import com.wilson.med.api.domain.medico.Especialidade;
+import com.wilson.med.api.domain.medico.Medico;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String login;
+	private String password;
+}
